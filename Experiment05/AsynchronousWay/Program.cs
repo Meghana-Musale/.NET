e72b621b86b996610d0561695b853a321924cfe7
Meghana-Musale <musalemeghana3@gmail.com>
2026-03-09 20:30:52 +0530

    Exp05 Commit

---
+++ Experiment05/AsynchronousWay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Work1()
+    {
+        Console.WriteLine("Work 1 Started");
+        await Task.Delay(2000);
+        Console.WriteLine("Work 1 Finished");
+    }
+
+    static async Task Work2()
+    {
+        Console.WriteLine("Work 2 Started");
+        await Task.Delay(2000);
+        Console.WriteLine("Work 2 Finished");
+    }
+
+    static async Task Main()
+    {
+        Task t1 = Work1();
+        Task t2 = Work2();
+
+        await Task.WhenAll(t1, t2);
+    }
+}
---
+++ Experiment05/SynchronousWay/Program.cs
@@ -0,0 +1,43 @@
+﻿/* 
+Synchronous Programming :
+    - meaning each step waits for the previous one to finish, no parallel threads, no async/await.
+      By default, C# runs synchronously, so unless you use async, Task, or threads, your code executes line by line.
+ */
+
+using System;
+
+class Program
+{
+    static void Step1()
+    {
+        Console.WriteLine("Step 1: Starting");
+        System.Threading.Thread.Sleep(1000); // simulate work for 1 second
+        Console.WriteLine("Step 1: Completed");
+    }
+
+    static void Step2()
+    {
+        Console.WriteLine("Step 2: Starting");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("Step 2: Completed");
+    }
+
+    static void Step3()
+    {
+        Console.WriteLine("Step 3: Starting");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("Step 3: Completed");
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Program Started");
+
+        Step1(); // runs first
+        Step2(); // waits until Step1 finishes
+        Step3(); // waits until Step2 finishes
+
+        Console.WriteLine("Program Finished");
+        Console.ReadLine(); // keep console open
+    }
+}
